9e8d3fce1ddd65154b29f428750b033ce48e75d3
Katlego Mkhonza <katlegoalpha@gmail.com>
2021-10-09 11:44:34 +0200

    implementing contains function

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -16,7 +16,16 @@ function same([x, y], [j, k]) {
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  
+  if(this===cell){
+    return this;
+  }
+  else {
+    return false;
+  }
+
+}
 
 const printCell = (cell, state) => {};
 
